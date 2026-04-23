3bd99cb27aba0cbdd75014582a263acc229b5025
deoskaizennox <164125215+deoskaizennox@users.noreply.github.com>
2026-04-23 06:16:38 +0200

    Removed trailing whitespace

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -5,7 +5,7 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main import app  # noqa: E402 
+from main import app  # noqa: E402
 
 
 @pytest.fixture
